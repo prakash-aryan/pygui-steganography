74783b42e217f64570a6ae1bf2c3077f3f219347
prakash-aryan <prakasharyan25@gmail.com>
2024-04-10 09:46:18 +0400

    Add styles for QMessageBox widgets

---
+++ ui/styles.py
@@ -0,0 +1,16 @@
+# styles.py
+message_box_style = """
+QMessageBox {
+    background-color: #282a36;
+    color: #f8f8f2;
+}
+QMessageBox QPushButton {
+    background-color: #44475a;
+    color: #f8f8f2;
+    padding: 5px;
+    border-radius: 3px;
+}
+QMessageBox QPushButton:hover {
+    background-color: #6272a4;
+}
+"""
