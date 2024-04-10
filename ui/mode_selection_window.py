b167382fb4846f4052448427c8feaf35c2d5653f
prakash-aryan <prakasharyan25@gmail.com>
2024-04-10 09:45:25 +0400

    Add flexible steganography type selection UI

--- ui/mode_selection_window.py
+++ ui/mode_selection_window.py
@@ -1,21 +1,16 @@
 from PyQt5.QtWidgets import QWidget
 from PyQt5.uic import loadUi
-from .encode_window import EncodeWindow
-from .decode_window import DecodeWindow
+from .steg_type_selection_window import StegTypeSelectionWindow
 
 class ModeSelectionWindow(QWidget):
     def __init__(self):
         super().__init__()
         loadUi('ui/mode_selection.ui', self)
-        self.btnEncode.clicked.connect(self.openEncodeWindow)
-        self.btnDecode.clicked.connect(self.openDecodeWindow)
+        self.btnEncode.clicked.connect(self.openStegTypeSelectionWindow)
+        self.btnDecode.clicked.connect(self.openStegTypeSelectionWindow)
 
-    def openEncodeWindow(self):
-        self.encodeWindow = EncodeWindow(self)
-        self.encodeWindow.show()
-        self.hide()
-
-    def openDecodeWindow(self):
-        self.decodeWindow = DecodeWindow(self)
-        self.decodeWindow.show()
+    def openStegTypeSelectionWindow(self):
+        mode = self.sender().text()
+        self.stegTypeSelectionWindow = StegTypeSelectionWindow(mode, self)
+        self.stegTypeSelectionWindow.show()
         self.hide()
