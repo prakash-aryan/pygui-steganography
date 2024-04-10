6f24cd1435552d858408499ff7f9ec170c15f0d4
prakash-aryan <prakasharyan25@gmail.com>
2024-04-10 09:47:33 +0400

    Implement custom styling for QMessageBox

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui.mode_selection_window import ModeSelectionWindow
+from ui.styles import message_box_style
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    # Apply the style sheet
+    app.setStyleSheet(message_box_style)
+
     modeSelectionWindow = ModeSelectionWindow()
     modeSelectionWindow.show()
+
     sys.exit(app.exec_())
